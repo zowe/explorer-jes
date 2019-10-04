60d4c82ce33c92f3b54fa868bbc89b0cf8812b0a
Jordan Cain <jordan.cain1@uk.ibm.com>
2019-10-04 15:57:36 -0400

    move all config to config.sh
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- scripts/jes-explorer-configure.sh
+++ scripts/jes-explorer-configure.sh
@@ -10,9 +10,11 @@
 # Copyright IBM Corporation 2019
 ################################################################################
 
-#********************************************************************
-# Expected globals:
-# STATIC_DEF_CONFIG_DIR
+################################################################################
+# Variables required on shell:
+# - STATIC_DEF_CONFIG_DIR
+# - ROOT_DIR
+# - NODE_HOME
 
 # Remove any old config
 rm ${STATIC_DEF_CONFIG_DIR}/jobs-ui.yml
@@ -33,6 +35,43 @@ services:
         serviceRelativeUrl: ui/v1/explorer-jes
 EOF
 
+if [ ! -z "$NODE_HOME" ]; then
+  NODE_BIN=${NODE_HOME}/bin/node
+else
+  echo "Error: cannot find node bin, JES Explorer UI is not configured."
+  exit 1
+fi
+
 iconv -f IBM-1047 -t IBM-850 ${STATIC_DEF_CONFIG_DIR}/jobs-ui.ebcdic.yml > $STATIC_DEF_CONFIG_DIR/jobs-ui.yml	
 rm ${STATIC_DEF_CONFIG_DIR}/jobs-ui.ebcdic.yml
 chmod 770 $STATIC_DEF_CONFIG_DIR/jobs-ui.yml
+
+cd "$ROOT_DIR/components/jes-explorer/bin"
+
+EXPLORER_PLUGIN_BASEURI=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.baseuri)")
+EXPLORER_PLUGIN_ID=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.pluginId)")
+EXPLORER_PLUGIN_NAME=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.pluginName)")
+
+if [ -z "$EXPLORER_PLUGIN_ID" ]; then
+  echo "  Error: cannot read plugin ID, install aborted."
+  exit 0
+fi
+if [ -z "$EXPLORER_PLUGIN_NAME" ]; then
+  echo "  Error: cannot read plugin name, install aborted."
+  exit 0
+fi
+if [ -z "$EXPLORER_PLUGIN_BASEURI" ]; then
+  echo "  Error: cannot read server base uri, install aborted."
+  exit 0
+fi
+
+# Add explorer plugin to zLUX 
+# NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
+EXPLORER_PLUGIN_FULLURL="https://${ZOWE_EXPLORER_HOST}:${GATEWAY_PORT}${EXPLORER_PLUGIN_BASEURI}"
+. $ROOT_DIR/scripts/configure/zowe-install-iframe-plugin.sh \
+    "$ROOT_DIR" \
+    "${EXPLORER_PLUGIN_ID}" \
+    "${EXPLORER_PLUGIN_NAME}" \
+    $EXPLORER_PLUGIN_FULLURL \
+    "${ROOT_DIR}/components/jes-explorer/bin/plugin-definition/zlux/images/explorer-JES.png" \
+    ${ROOT_DIR}/components/jes-explorer/
--- scripts/jes-explorer-start.sh
+++ scripts/jes-explorer-start.sh
@@ -16,8 +16,12 @@
 # - ROOT_DIR
 # - ZOWE_EXPLORER_HOST
 # - GATEWAY_PORT
+# - JES_EXPLORER_UI_PORT
+# - KEYSTORE_KEY
+# - KEYSTORE_CERTIFICATE
+# - KEYSTORE_PASSWORD
 
-# find node bin
+# Find node binary
 if [ ! -z "$NODE_HOME" ]; then
   NODE_BIN=${NODE_HOME}/bin/node
 else
@@ -25,35 +29,8 @@ else
   exit 1
 fi
 
-# NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
 cd "$ROOT_DIR/components/jes-explorer/bin"
-
-EXPLORER_PLUGIN_BASEURI=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.baseuri)")
-EXPLORER_PLUGIN_ID=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.pluginId)")
 EXPLORER_PLUGIN_NAME=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.pluginName)")
-
-if [ -z "$EXPLORER_PLUGIN_ID" ]; then
-  echo "  Error: cannot read plugin ID, install aborted."
-  exit 0
-fi
-if [ -z "$EXPLORER_PLUGIN_NAME" ]; then
-  echo "  Error: cannot read plugin name, install aborted."
-  exit 0
-fi
-if [ -z "$EXPLORER_PLUGIN_BASEURI" ]; then
-  echo "  Error: cannot read server base uri, install aborted."
-  exit 0
-fi
-
-# Add explorer plugin to zLUX 
-EXPLORER_PLUGIN_FULLURL="https://${ZOWE_EXPLORER_HOST}:${GATEWAY_PORT}${EXPLORER_PLUGIN_BASEURI}"
-. $ROOT_DIR/scripts/configure/zowe-install-iframe-plugin.sh \
-    "$ROOT_DIR" \
-    "${EXPLORER_PLUGIN_ID}" \
-    "${EXPLORER_PLUGIN_NAME}" \
-    $EXPLORER_PLUGIN_FULLURL \
-    "${ROOT_DIR}/components/jes-explorer/bin/plugin-definition/zlux/images/explorer-JES.png" \
-    ${ROOT_DIR}/components/jes-explorer/
 
 # get current ui server directory
 SERVER_DIR="${ROOT_DIR}/components/jes-explorer/bin/server/"
