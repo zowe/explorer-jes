85f28cc45277505c2d06d284f0b7c3c3f3e2ca02
Nakul Manchanda <nakul.manchanda@ibm.com>
2019-10-10 11:53:07 -0400

    moved iframe plugin install to zowe-configure
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- scripts/jes-explorer-configure.sh
+++ scripts/jes-explorer-configure.sh
@@ -48,32 +48,3 @@ iconv -f IBM-1047 -t IBM-850 ${STATIC_DEF_CONFIG_DIR}/jobs-ui.ebcdic.yml > $STAT
 rm ${STATIC_DEF_CONFIG_DIR}/jobs-ui.ebcdic.yml
 chmod 770 $STATIC_DEF_CONFIG_DIR/jobs-ui.yml
 
-cd "$ROOT_DIR/components/jes-explorer/bin"
-
-EXPLORER_PLUGIN_BASEURI=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.baseuri)")
-EXPLORER_PLUGIN_ID=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.pluginId)")
-EXPLORER_PLUGIN_NAME=$($NODE_BIN -e "process.stdout.write(require('./package.json').config.pluginName)")
-
-if [ -z "$EXPLORER_PLUGIN_ID" ]; then
-  echo "  Error: cannot read plugin ID, install aborted."
-  exit 0
-fi
-if [ -z "$EXPLORER_PLUGIN_NAME" ]; then
-  echo "  Error: cannot read plugin name, install aborted."
-  exit 0
-fi
-if [ -z "$EXPLORER_PLUGIN_BASEURI" ]; then
-  echo "  Error: cannot read server base uri, install aborted."
-  exit 0
-fi
-
-# Add explorer plugin to zLUX 
-# NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
-EXPLORER_PLUGIN_FULLURL="https://${ZOWE_EXPLORER_HOST}:${GATEWAY_PORT}${EXPLORER_PLUGIN_BASEURI}"
-. $ROOT_DIR/scripts/configure/zowe-install-iframe-plugin.sh \
-    "$ROOT_DIR" \
-    "${EXPLORER_PLUGIN_ID}" \
-    "${EXPLORER_PLUGIN_NAME}" \
-    $EXPLORER_PLUGIN_FULLURL \
-    "${ROOT_DIR}/components/jes-explorer/bin/plugin-definition/zlux/images/explorer-JES.png" \
-    ${ROOT_DIR}/components/jes-explorer/
