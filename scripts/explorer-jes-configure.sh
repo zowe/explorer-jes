bc21a72443c8e8c03bf03cfc508f15f433a3f1e7
stevenhorsman <steven@uk.ibm.com>
2020-01-07 16:06:20 +0000

    Create iframe in config
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/explorer-jes-configure.sh
+++ scripts/explorer-jes-configure.sh
@@ -7,7 +7,7 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# Copyright IBM Corporation 2019
+# Copyright IBM Corporation 2019, 2020
 ################################################################################
 
 ################################################################################
@@ -18,12 +18,27 @@
 
 NODE_BIN=${NODE_HOME}/bin/node
 
+
 EXPLORER_CONFIG="$ROOT_DIR/components/explorer-jes/bin/app/package.json"
 EXPLORER_PLUGIN_BASEURI=$($NODE_BIN -e "process.stdout.write(require('${EXPLORER_CONFIG}').config.baseuri)")
+EXPLORER_PLUGIN_ID=$($NODE_BIN -e "process.stdout.write(require('${EXPLORER_CONFIG}').config.pluginId)")
+EXPLORER_PLUGIN_NAME=$($NODE_BIN -e "process.stdout.write(require('${EXPLORER_CONFIG}').config.pluginName)")
 
-# Remove any old config
+if [[ $LAUNCH_COMPONENT_GROUPS == *"DESKTOP"* ]]
+then
+  # Create desktop app plugin
+  EXPLORER_PLUGIN_FULLURL="https://${ZOWE_EXPLORER_HOST}:${GATEWAY_PORT}${EXPLORER_PLUGIN_BASEURI}"
+  ${ROOT_DIR}/bin/utils/zowe-install-iframe-plugin.sh \
+    "${EXPLORER_PLUGIN_ID}" \
+    "${EXPLORER_PLUGIN_NAME}" \
+    ${EXPLORER_PLUGIN_FULLURL} \
+    "${WORKSPACE_DIR}/explorer-jes" \
+    "${ROOT_DIR}/components/explorer-jes/bin/app/img/explorer-JES.png"
+fi
+
+# Remove any previous static defintiion config
 if [[ -f ${STATIC_DEF_CONFIG_DIR}/jobs-ui.yml ]]; then
-    rm ${STATIC_DEF_CONFIG_DIR}/jobs-ui.yml 
+  rm ${STATIC_DEF_CONFIG_DIR}/jobs-ui.yml 
 fi
 
 # Add static definition for jes explorer ui
