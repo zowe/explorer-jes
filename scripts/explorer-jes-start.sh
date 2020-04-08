86add743fa42538af71d317386936b6f8c775fc6
Jordan Cain <jordan.cain1@uk.ibm.com>
2020-04-08 15:21:09 -0400

    Add bpx jobname to start script
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- scripts/explorer-jes-start.sh
+++ scripts/explorer-jes-start.sh
@@ -7,7 +7,7 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# Copyright IBM Corporation 2018, 2019
+# Copyright IBM Corporation 2018, 2020
 ################################################################################
 
 ################################################################################
@@ -15,11 +15,10 @@
 # - NODE_HOME
 # - ROOT_DIR
 # - ZOWE_EXPLORER_HOST
-# - GATEWAY_PORT
 # - JES_EXPLORER_UI_PORT
 # - KEYSTORE_KEY
 # - KEYSTORE_CERTIFICATE
-# - KEYSTORE_PASSWORD
+# - ZOWE_PREFIX
 
 NODE_BIN=${NODE_HOME}/bin/node
 
@@ -30,8 +29,14 @@ EXPLORER_PLUGIN_NAME=$($NODE_BIN -e "process.stdout.write(require('./app/package
 # get current ui server directory
 SERVER_DIR="${ROOT_DIR}/components/explorer-jes/bin/server/"
 
+if [ -z "$ZOWE_PREFIX" ]
+then
+  ZOWE_PREFIX="ZWE"
+fi
+JOB_NAME="${ZOWE_PREFIX}UJ"
+
 # start service
-$NODE_BIN $SERVER_DIR/src/index.js \
+_BPX_JOBNAME=${JOB_NAME} $NODE_BIN $SERVER_DIR/src/index.js \
   --service ${EXPLORER_PLUGIN_NAME} \
 	--path ${EXPLORER_PLUGIN_BASEURI} \
 	--port ${JES_EXPLORER_UI_PORT} \
