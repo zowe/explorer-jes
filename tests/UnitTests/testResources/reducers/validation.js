c4e218585ad94dcc43643965cdca9ee6d87407c2
Jordan Cain <jordan.cain1@uk.ibm.com>
2020-05-28 13:26:42 -0400

    Add a force login flag if a jobs request comes back as 401
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- tests/UnitTests/testResources/reducers/validation.js
+++ tests/UnitTests/testResources/reducers/validation.js
@@ -15,6 +15,7 @@ export const baseValidation = Map({
     isValidating: false,
     username: '',
     message: '',
+    forceLogin: false,
 });
 
 export const requestedValidation = Map({
@@ -22,6 +23,7 @@ export const requestedValidation = Map({
     isValidating: true,
     username: '',
     message: '',
+    forceLogin: false,
 });
 
 export const receivedValidation = Map({
@@ -29,6 +31,7 @@ export const receivedValidation = Map({
     isValidating: false,
     username: 'dummyUser',
     message: '',
+    forceLogin: false,
 });
 
 export const invalidatedValidation = Map({
@@ -36,6 +39,15 @@ export const invalidatedValidation = Map({
     isValidating: false,
     username: 'dummyUser',
     message: '',
+    forceLogin: true,
+});
+
+export const invalidatedValidationNoUser = Map({
+    validated: false,
+    isValidating: false,
+    username: '',
+    message: '',
+    forceLogin: true,
 });
 
 export const receivedSpecialCharsValidation = Map({
@@ -43,4 +55,5 @@ export const receivedSpecialCharsValidation = Map({
     isValidating: false,
     username: "!@£$%^&*&^%$£@'test'{C}<I>[C]`S`汉语/漢語Wałęsa æøå",
     message: '',
+    forceLogin: false,
 });
