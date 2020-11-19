66b90fbd74e75ef9ff758bb0ac50b37f30f5544d
stevenhorsman <steven@uk.ibm.com>
2020-11-19 11:28:34 +0000

    Review comments
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- .pax/prepare-workspace.sh
+++ .pax/prepare-workspace.sh
@@ -54,10 +54,10 @@ sed -e "s|{{build\.branch}}|${BRANCH_NAME}|g" \
     -e "s|{{build\.number}}|${BUILD_NUMBER}|g" \
     -e "s|{{build\.commitHash}}|${commit_hash}|g" \
     -e "s|{{build\.timestamp}}|${current_timestamp}|g" \
-    "${PAX_WORKSPACE_DIR}/manifest.yaml" > "${PAX_WORKSPACE_DIR}/manifest.yaml.tmp"
-mv "${PAX_WORKSPACE_DIR}/manifest.yaml.tmp" "${PAX_WORKSPACE_DIR}/manifest.yaml"
+    "${PAX_WORKSPACE_DIR}/content/manifest.yaml" > "${PAX_WORKSPACE_DIR}/content/manifest.yaml.tmp"
+mv "${PAX_WORKSPACE_DIR}/content/manifest.yaml.tmp" "${PAX_WORKSPACE_DIR}/content/manifest.yaml"
 echo "[${SCRIPT_NAME}] manifest:"
-cat "${PAX_WORKSPACE_DIR}/manifest.yaml"
+cat "${PAX_WORKSPACE_DIR}/content/manifest.yaml"
 
 # copy start scripts to target folder
 echo "[${SCRIPT_NAME}] copying startup scripts ..."
