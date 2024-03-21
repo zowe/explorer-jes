11173ddb19ca56528137c6a925547cfce840c21f
1000TurquoisePogs <sgrady@rocketsoftware.com>
2024-03-21 08:36:38 -0400

    Fix for linter
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- WebContent/js/utilities/urlUtils.js
+++ WebContent/js/utilities/urlUtils.js
@@ -25,7 +25,7 @@ const ZOSMF_PREFIX_LENGTH = 6;
 
 export function atlasFetch(endpoint, content) {
     // In v3, /ibmzosmf/api/v1 endpoint removes /zosmf part of a /zosmf URL, so string must be trimmed.
-    let trimmedEndpoint = endpoint.substring(ZOSMF_PREFIX_LENGTH);
+    const trimmedEndpoint = endpoint.substring(ZOSMF_PREFIX_LENGTH);
 
     return fetch(`https://${whichServer()}/ibmzosmf/api/v1/${trimmedEndpoint}`, content);
 }
