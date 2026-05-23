b8f52594f1374044a13baadf718009667e5a4f72
1000TurquoisePogs <sgrady@rocketsoftware.com>
2026-05-23 11:32:12 +0200

    Remove rewire, sinon
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- WebContent/js/actions/jobNodes.ts
+++ WebContent/js/actions/jobNodes.ts
@@ -40,15 +40,15 @@ export const RECEIVE_PURGE_JOB = 'RECEIVE_PURGE_JOB';
 export const INVALIDATE_PURGE_JOB = 'INVALIDATE_PURGE_JOB';
 
 const NO_JOBS_FOUND_MESSAGE = 'No Jobs found for filter parameters';
-const CANCEL_JOB_SUCCESS_MESSAGE = 'Cancel request succeeded for';
-const CANCEL_JOB_FAIL_MESSAGE = 'Cancel request failed for';
-const CANCEL_JOB_CANCEL_MESSAGE = 'Cancel request canceled for';
-const PURGE_JOB_SUCCESS_MESSAGE = 'Purge request succeeded for';
-const PURGE_JOBS_SUCCESS_MESSAGE = 'Purge request succeeded for selected jobs';
-const PURGE_JOB_CANCEL_MESSAGE = 'Purge request canceled for';
-const PURGE_JOBS_CANCEL_MESSAGE = 'Purge request canceled for selected jobs';
-const PURGE_JOB_FAIL_MESSAGE = 'Purge request failed for';
-const PURGE_JOBS_FAIL_MESSAGE = 'Purge request failed for the following selected jobs';
+export const CANCEL_JOB_SUCCESS_MESSAGE = 'Cancel request succeeded for';
+export const CANCEL_JOB_FAIL_MESSAGE = 'Cancel request failed for';
+export const CANCEL_JOB_CANCEL_MESSAGE = 'Cancel request canceled for';
+export const PURGE_JOB_SUCCESS_MESSAGE = 'Purge request succeeded for';
+export const PURGE_JOBS_SUCCESS_MESSAGE = 'Purge request succeeded for selected jobs';
+export const PURGE_JOB_CANCEL_MESSAGE = 'Purge request canceled for';
+export const PURGE_JOBS_CANCEL_MESSAGE = 'Purge request canceled for selected jobs';
+export const PURGE_JOB_FAIL_MESSAGE = 'Purge request failed for';
+export const PURGE_JOBS_FAIL_MESSAGE = 'Purge request failed for the following selected jobs';
 
 function requestJobs(filters) {
     return {
@@ -199,7 +199,7 @@ function invalidatePurge(jobName, jobId) {
     };
 }
 
-function getURIQuery(filters) {
+export function getURIQuery(filters) {
     let query = `?owner=${filters.owner ? encodeURLComponent(filters.owner) : '*'}&prefix=${filters.prefix ? encodeURLComponent(filters.prefix) : '*'}`;
 
     if (filters.status && filters.status !== '*') {
